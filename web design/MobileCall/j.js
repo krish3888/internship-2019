56a628eaab4ef2c4f02984d594d69016f1e6921b
Krishna Shahi <krishnashahi3888@gmail.com>
2019-06-24 10:39:14 +0530

    mobileApp

---
+++ web design/MobileCall/j.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+	
+	$("#contactlist").click(function(){
+
+		$("#front").css("display","none");
+		$("#next").css("display","block");
+	})
+	$("#cut").click(function(){
+
+		$("#front").css("display","block");
+		$("#next").css("display","none");
+	})
+	$(".key").on("click",function(){
+		if ($(this).is("#btn1")){
+			$("#in").val(($("#in").val()+"1"));
+		}
+		else if ($(this).is("#btn2")){
+			$("#in").val(($("#in").val()+"2"));
+		}
+		else if ($(this).is("#btn3")){
+			$("#in").val(($("#in").val()+"3"));
+		}
+		else if ($(this).is("#btn4")){
+		$("#in").val(($("#in").val()+"4"));
+		}
+		else if ($(this).is("#btn5")){
+		    $("#in").val(($("#in").val()+"5"));
+		}
+		else if ($(this).is("#btn6")){
+		    $("#in").val(($("#in").val()+"6"));
+		}
+		else if ($(this).is("#btn7")){
+		    $("#in").val(($("#in").val()+"7"));
+		}
+		else if ($(this).is("#btn8")){
+		    $("#in").val(($("#in").val()+"8"));
+		}
+		else if ($(this).is("#btn9")){
+		    $("#in").val(($("#in").val()+"9"));
+		}
+		else if ($(this).is("#btn11")){
+		    $("#in").val(($("#in").val()+"*"));
+		}
+		else if ($(this).is("#btn0")){
+		    $("#in").val(($("#in").val()+"0"));
+		}
+		else if ($(this).is("#btn12")){
+		    $("#in").val(($("#in").val()+"#"));
+		}
+	})
+	$("#callbtn").click(function()){
+		$("#front").css("display","none");
+		$("#n Next").css("display","block");
+	})         )
+	}
+});
